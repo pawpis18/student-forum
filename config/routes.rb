2343a250a8a7f683bded7c3eae1b085d5c27be9b
pawpis18 <student@localhost.localdomain>
2017-10-16 16:52:52 +0200

    na zakoncenei zajec

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :topics
+  resources :courses
+  resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
